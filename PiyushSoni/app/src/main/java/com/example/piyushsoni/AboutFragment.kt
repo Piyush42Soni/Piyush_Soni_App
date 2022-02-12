5539df3381cd1b9c59855c58ce57776c4191f23d
Piyush Soni <piyush42soni@gmail.com>
2022-02-12 14:50:25 +0530

    commited

--- PiyushSoni/app/src/main/java/com/example/piyushsoni/AboutFragment.kt
+++ PiyushSoni/app/src/main/java/com/example/piyushsoni/AboutFragment.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.Transformation
+import android.widget.ProgressBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.piyushsoni.databinding.FragmentAboutBinding
@@ -13,6 +16,19 @@ import com.example.piyushsoni.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
     private lateinit var binding:FragmentAboutBinding
+
+    class ProgressBarAnimation(
+        private val progressBar: ProgressBar,
+        private val from: Float,
+        private val to: Float
+    ) :
+        Animation() {
+        override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+            super.applyTransformation(interpolatedTime, t)
+            val value = from + (to - from) * interpolatedTime
+            progressBar.progress = value.toInt()
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,13 +60,52 @@ class AboutFragment : Fragment() {
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
         }
-        binding.hakerrankButton.setOnClickListener{
+        binding.hakerrankButton.setOnClickListener {
             val uri: Uri =
                 Uri.parse("https://www.hackerrank.com/piyush42soni")
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
         }
-
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var anim = ProgressBarAnimation(binding.progressBar, 0F, 95F)
+        anim.duration = 3000
+        binding.progressBar.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar1, 0F, 80F)
+        anim.duration = 3000
+        binding.progressBar1.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar2, 0F, 80F)
+        anim.duration = 3000
+        binding.progressBar2.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar3, 0F, 80F)
+        anim.duration = 3000
+        binding.progressBar3.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar4, 0F, 95F)
+        anim.duration = 3000
+        binding.progressBar4.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar5, 0F, 90F)
+        anim.duration = 3000
+        binding.progressBar5.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar6, 0F, 90F)
+        anim.duration = 3000
+        binding.progressBar6.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar7, 0F, 65F)
+        anim.duration = 3000
+        binding.progressBar7.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar8, 0F, 70F)
+        anim.duration = 3000
+        binding.progressBar8.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar9, 0F, 90F)
+        anim.duration = 3000
+        binding.progressBar9.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar10, 0F, 90F)
+        anim.duration = 3000
+        binding.progressBar10.startAnimation(anim)
+        anim = ProgressBarAnimation(binding.progressBar11, 0F, 75F)
+        anim.duration = 3000
+        binding.progressBar11.startAnimation(anim)
     }
 }
